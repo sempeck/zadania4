74f8151db58878b6bf35cbd3aa5572e40421911f
sempeck <jarek.sepek@mediorama.pl>
2015-11-15 16:20:30 +0100

    Zadanie 1d

---
+++ zad1d.pas
@@ -0,0 +1,32 @@
+program zad1d;
+
+var
+	n, i, j: Integer;
+	tn : Char;
+
+begin
+
+  repeat
+  	
+	  repeat 
+		  writeln('Podaj N (od 1 do 15):');
+		  readln(n);
+	  until (n < 16) and (n > 0);
+
+		for i := n downto 1 do
+		  begin
+		    for j := 1 to n-i do
+		      write(' ');
+		      
+            for j := 1 to i * 2 - 1 do
+              write('*');
+		      
+		    writeln; 
+		  end;
+
+    writeln('Jeszcze raz? (t/n)');
+	  readln(tn);
+
+  until tn = 'n';
+
+end.
